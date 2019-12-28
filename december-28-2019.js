0373ae56b7c2456ee46177339c198d6daf034ab6
BenDehlin <871078+BenDehlin@users.noreply.github.com>
2019-12-28 16:49:14 -0700

    Update december-28-2019.js
    added missingValue solution, WIP.

--- december-28-2019.js
+++ december-28-2019.js
@@ -13,3 +13,21 @@ function addLetters(...letters) {
 }
 
 console.log(addLetters('a', 'b'))
+
+function missingValue(array){
+  let arr = []
+  let test = count2(array)
+  console.log(test)
+  for(let key in test){
+    if(test[key] === 1 || test[key] === 2){
+      arr.push([Number(key), test[key]])
+    }
+  }
+  return arr[0][0]*arr[0][1] + arr[1][0]*arr[1][1]
+}
+
+let count = (arr) => arr.reduce((x, y, z) => {
+  return (x[y] = x[y] + 1 || 1) && x
+  }, {})
+
+console.log(missingValue([1, 1, 1, 2, 2, 2, 3, 4, 4]))
