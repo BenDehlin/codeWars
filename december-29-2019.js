125d948032c218609251f260ee828c09c7cc6c15
BenDehlin <871078+BenDehlin@users.noreply.github.com>
2019-12-29 12:35:56 -0700

    Update and rename december-29-2019 to december-29-2019.js
    Added minecraft iron bars solution and isValidWalk solution

---
+++ december-29-2019.js
@@ -0,0 +1,87 @@
+function evenForLoop(arr){
+  let even =[]
+  let odd = []
+  for(let i = 0; i < arr.length; i++){
+    arr[i] % 2 ? odd.push(arr[i]) : even.push(arr[i])
+  }
+  return [even, odd]
+}
+
+function evenForLoop2(arr){
+  let even = []
+  let odd = []
+  for(let x of arr){
+    x % 2 ? odd.push(x) : even.push(x)
+  }
+  return [even, odd]
+}
+
+function evenForEach(arr){
+  let even = []
+  let odd = []
+  arr.forEach(x => x % 2 ? odd.push(x) : even.push(x))
+  return [even, odd]
+}
+console.log(evenForLoop([1, 4, 3, 3, 2, 5, 2]))
+console.log(evenForLoop2([1, 4, 3, 3, 2, 5, 2]))
+console.log(evenForEach([1, 4, 3, 3, 2, 5, 2]))
+
+
+function calcFuel(rods){
+  let time = 11 * rods
+  let obj = {
+    lava: 0,
+    blazeRod: 0,
+    coal: 0,
+    wood: 0,
+    stick: 0
+  }
+  return recurseFuel(time, obj)
+}
+
+function recurseFuel(time, object){
+  if(time >= 800){
+    object.lava += 1
+    time -= 800
+  }else if(time >= 120){
+    object.blazeRod +=1
+    time -= 120
+  }else if(time >= 80){
+    object.coal +=1
+    time -= 80
+  }else if(time >= 15){
+    object.wood +=1
+    time -= 15
+  }else if(time >= 1){
+    object.stick +=1
+    time -= 1
+  }if (time > 0){
+    recurseFuel(time, object)
+  }
+  return object
+}
+console.log(calcFuel(50))
+
+
+function isValidWalk(directions){
+  let location = [0, 0]
+  if(directions.length !== 10){
+  return false
+  }
+  for(i = 0; i < directions.length; i++){
+    if(directions[i] === 'n'){
+      location[1] += 1
+    }
+    if(directions[i] === 'w'){
+      location[0] -= 1      
+    }
+    if(directions[i] === 's'){
+      location[1] -= 1
+    }
+    if(directions[i] === 'e'){
+      location[0] += 1
+    }
+  }
+  return location[0] === 0 && location[1] === 0 ? true : false
+}
+console.log(isValidWalk(['n', 's', 'w', 'e']))
