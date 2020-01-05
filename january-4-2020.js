dd931820f493239204e44db1efdf4c197259b9df
BenDehlin <871078+BenDehlin@users.noreply.github.com>
2020-01-04 22:06:20 -0700

    Update and rename january-4-2020 to january-4-2020.js
    added absentVowel, generateRange, and compareArray

--- january-4-2020.js
+++ january-4-2020.js
@@ -51,3 +51,41 @@ function digital_root(num){
 }
 
 digital_root(17886723)
+
+function generateRange(min, max, step){
+  arr = []
+  for(let i = min; i <= max; i+=step){
+    arr.push(i)
+  }
+  return arr
+}
+
+console.log(generateRange(4, 7, 3))
+
+function absentVowel(str){
+  if(!/[a]/i.test(str)){
+    return 0
+  }
+    if(!/[e]/i.test(str)){
+    return 1
+  }
+    if(!/[i]/i.test(str)){
+    return 2
+  }
+    if(!/[o]/i.test(str)){
+    return 3
+  }
+    if(!/[u]/i.test(str)){
+    return 4
+  }
+}
+console.log(absentVowel('Bb Smith sent us six neatly arranged range bicycles'))
+
+function comp(arr1, arr2){
+  if(arr1 === null || arr2 === null){return false}
+  arr1Sort = arr1.map(element => Math.pow(element, 2)).sort()
+  return arr2.sort().every((element, index) => element === arr1Sort[index])
+}
+
+console.log(comp([1, 2, 3, 4], [4, 16, 9, 1]))
+
